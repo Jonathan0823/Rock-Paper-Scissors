e3ce9715034e3560e598fa3782be8ea8fa24328c
Guinn <eguinjonathan08@gmail.com>
2024-09-13 15:16:09 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,43 @@
+import random
+
+option = ["rock", "paper", "scissors"]
+guess = ""
+repeat = ""
+wins = 0
+tries = 0
+
+print("Rock, Paper, Scissors Game")
+while True:
+    if repeat == "n":
+        print("=========Score=========")
+        print(f"Tries: {tries}")
+        print(f"Your total wins: {wins}")
+        break
+    else:
+        answer = random.choice(option)
+        guess = input("\nEnter your choice (rock, paper, scissors): ").lower()
+        if guess in option:
+            if  guess == answer:
+                print("Draw")
+            elif guess == "rock" and answer == "paper":
+                print("You lose")
+            elif guess == "rock" and answer == "scissors":
+                print("You win")
+                wins += 1
+            elif guess == "paper" and answer == "rock":
+                print("You win")
+                wins += 1
+            elif guess == "paper" and answer == "scissors":
+                print("You lose")
+            elif guess == "scissors" and answer == "rock":
+                print("You lose")
+            elif guess == "scissors" and answer == "paper":
+                print("You win")
+                wins += 1
+            tries += 1
+        else:
+            print("Invalid input")
+        repeat = input("Would you like to play again? (y/n): ").lower()
+
+        
+
