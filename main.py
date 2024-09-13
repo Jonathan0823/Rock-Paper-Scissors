bc76464aba1699208a0330c65ce93d20fbde4a45
Guinn <eguinjonathan08@gmail.com>
2024-09-13 15:22:03 +0700

    print player's and computer's answer

--- main.py
+++ main.py
@@ -18,20 +18,27 @@ while True:
         guess = input("\nEnter your choice (rock, paper, scissors): ").lower()
         if guess in option:
             if  guess == answer:
+                print(f"Player: {guess}, Computer: {answer}")
                 print("Draw")
             elif guess == "rock" and answer == "paper":
+                print(f"Player: {guess}, Computer: {answer}")
                 print("You lose")
             elif guess == "rock" and answer == "scissors":
+                print(f"Player: {guess}, Computer: {answer}")
                 print("You win")
                 wins += 1
             elif guess == "paper" and answer == "rock":
+                print(f"Player: {guess}, Computer: {answer}")   
                 print("You win")
                 wins += 1
             elif guess == "paper" and answer == "scissors":
+                print(f"Player: {guess}, Computer: {answer}")
                 print("You lose")
             elif guess == "scissors" and answer == "rock":
+                print(f"Player: {guess}, Computer: {answer}")
                 print("You lose")
             elif guess == "scissors" and answer == "paper":
+                print(f"Player: {guess}, Computer: {answer}")
                 print("You win")
                 wins += 1
             tries += 1
